8ac857563a88e1af9b29ba76c984800b243ba34e
TJ03 <tj03@TJ03ui-MacBookPro.local>
2021-05-04 16:59:59 +0900

    post 수정

--- src/main/java/com/tjint/springboot/app/api/admin/controller/adminLoginApi.java
+++ src/main/java/com/tjint/springboot/app/api/admin/controller/adminLoginApi.java
@@ -5,7 +5,7 @@ import com.tjint.springboot.common.UserInfoVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ public class adminLoginApi {
     private final AdminLoginApiService adminLoginApiService;
 
     @ApiOperation(value = "회원 조회", notes = "회원을 조회한다.")
-    @GetMapping(value = "/adminUser")
+    @PostMapping(value = "/adminUser")
     public List<UserInfoVo> getUserList() throws Exception {
         List<UserInfoVo> userInfoList = this.adminLoginApiService.getUserList();
 
